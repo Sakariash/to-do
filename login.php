a450e2ef80a418cc51f16f868e1f2c5a4f76c472
Sakariash <simonsakarias@gmail.com>
2022-01-13 15:28:26 +0100

    left a few to many div tags that messed up the ui

--- login.php
+++ login.php
@@ -1,41 +1,41 @@
 <?php require __DIR__ . '/navigation.php' ?>
 
-<?php endif; ?>
 <div class="content-panel">
     <h1 class="title">Welcome!</h1>
     <h2 class="fieldset-title">Log in</h2>
-    <?php if (isset($_SESSION['errors'])) :
-        foreach ($_SESSION['errors'] as $error) : ?>
-            <p class="alert alert-danger"><?php echo htmlspecialchars($error); ?></p>
-        <?php endforeach; ?>
-        <?php unset($_SESSION['errors']); ?>
-    <?php endif; ?>
-    <?php if (isset($_SESSION['confirm'])) :
-        foreach ($_SESSION['confirm'] as $confirmUser) : ?>
-            <p class="alert alert-success"><?php echo htmlspecialchars($confirmUser); ?></p>
-        <?php endforeach; ?>
-        <?php unset($_SESSION['confirm']); ?>
-    <?php endif; ?>
-    <form action="app/users/login.php" method="post" class="form-horizontal">
-        <div class="form-group">
-            <label class="col-md-2 col-sm-3 col-xs-12 control-label">Email</label>
-            <div class="col-md-10 col-sm-9 col-xs-12">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
-                <small class="form-text">Please provide your email adress.</small>
+    <fieldset class="fieldset">
+        <?php if (isset($_SESSION['errors'])) :
+            foreach ($_SESSION['errors'] as $error) : ?>
+                <p class="alert alert-danger"><?php echo htmlspecialchars($error); ?></p>
+            <?php endforeach; ?>
+            <?php unset($_SESSION['errors']); ?>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['confirm'])) :
+            foreach ($_SESSION['confirm'] as $confirmUser) : ?>
+                <p class="alert alert-success"><?php echo htmlspecialchars($confirmUser); ?></p>
+            <?php endforeach; ?>
+            <?php unset($_SESSION['confirm']); ?>
+        <?php endif; ?>
+        <form action="app/users/login.php" method="post" class="form-horizontal">
+            <div class="form-group">
+                <label class="col-md-2 col-sm-3 col-xs-12 control-label">Email</label>
+                <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                    <small class="form-text">Please provide your email adress.</small>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="col-md-2 col-sm-3 col-xs-12 control-label">Password</label>
-            <div class="col-md-10 col-sm-9 col-xs-12">
-                <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-                <small class="form-text">Please provide your password.</small>
+            <div class="form-group">
+                <label class="col-md-2 col-sm-3 col-xs-12 control-label">Password</label>
+                <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                    <small class="form-text">Please provide your password.</small>
+                </div>
             </div>
-        </div>
-        <hr>
-        <div class="form-group">
-            <div class="col-md-10 col-sm-9 col-xs-12 col-md-push-2 col-sm-push-3 col-xs-push-0">
-                <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+            <hr>
+            <div class="form-group">
+                <div class="col-md-10 col-sm-9 col-xs-12 col-md-push-2 col-sm-push-3 col-xs-push-0">
+                    <button type="submit" class="btn btn-primary">Login</button>
+        </form>
 </div>
 </div>
 </div>
